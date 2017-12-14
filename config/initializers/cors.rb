3723967842fee2b2631b251ff5e58fdf15a877e9
Matt Langan <matt.langan@austintexas.gov>
2017-12-14 09:10:34 -0600

    Whitelist localhost on production
    This allows for easier client-side testing by letting devs interact with the microservices without actually running them locally. Once the microservices are re-deployed as a docker container this will no longer be necessary.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'http://localhost:3001'
     elsif Rails.env.production?
-      origins 'austintexas.gov', 'https://coa-test-form-client.herokuapp.com'
+      origins 'austintexas.gov', 'https://coa-test-form-client.herokuapp.com', 'http://localhost:3000'
     end
     resource '*',
       headers: :any,
