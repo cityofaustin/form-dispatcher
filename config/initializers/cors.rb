af7db779505f13c14457a96b1531af32172bc7b5
Brian Smith <briaguya@gmail.com>
2019-10-30 16:29:00 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'http://localhost:3001'
     elsif Rails.env.production?
-      origins 'http://localhost:3000', 'http://janis-austin-gov-staging.s3-website-us-east-1.amazonaws.com', 'http://janis.austintexas.io', 'https://janis-3147-implement-feedback-comp.netlify.com', 'austintexas.gov', 'https://alpha.austin.gov', 'https://coa-test-form-client.herokuapp.com', '*.palmereventscenter.com', '*.austinconventioncenter.com'
+      origins 'http://localhost:3000', 'http://janis-austin-gov-staging.s3-website-us-east-1.amazonaws.com', 'https://janis.austintexas.io', 'https://janis-3147-implement-feedback-comp.netlify.com', 'austintexas.gov', 'https://alpha.austin.gov', 'https://coa-test-form-client.herokuapp.com', '*.palmereventscenter.com', '*.austinconventioncenter.com'
     end
     resource '*',
       headers: :any,
